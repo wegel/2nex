616fcdf65ede05591e476bbf92390ce3830e91a8
Simon Labrecque <simon@wegel.ca>
2025-12-03 12:15:10 -0500

    cli: use zub-store crate, remove in-tree src/zub

--- src/zub/src/object/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod blob;
-pub mod commit;
-pub mod tree;
-
-pub use blob::{blob_exists, blob_path, read_blob, write_blob};
-pub use commit::{commit_path, read_commit, write_commit};
-pub use tree::{read_tree, tree_path, write_tree};
--- src/zub/src/types/mod.rs
+++
@@ -1,7 +0,0 @@
-mod commit;
-mod metadata;
-mod tree;
-
-pub use commit::Commit;
-pub use metadata::{ChangeKind, DiffEntry, SparseRegion, Xattr};
-pub use tree::{EntryKind, Tree, TreeEntry};
